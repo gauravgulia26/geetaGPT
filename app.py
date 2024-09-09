527782d7b33ea74035736132082ded3ee250046e
gauravgulia26 <grvgulia007@gmail.com>
2024-09-09 21:21:27 +0530

    changed requirement.txt

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 # %%
 import os
-import time
-import pyttsx3
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_groq import ChatGroq
 from langchain_community.vectorstores import FAISS
 import streamlit as st
-
 st.set_page_config(layout="wide")
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
-from streamlit_extras.streaming_write import write
 
 
 load_dotenv()
